d5e957f2c528004f6b8c847c90432c474734658d
Szymon Stanisław Grzesiak <szymongrzesiak.pl@gmail.com>
2024-06-11 23:10:53 +0200

    add changes to layout

---
+++ app/(protected)/(home)/add/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Add = () => {
+  return (
+    <div className="drop-shadow-lg flex gap-4">
+      <div className="bg-white rounded-lg w-[300px] h-[300px]">Card1</div>
+      <div className="bg-white rounded-lg w-[300px] h-[300px]">Card2</div>
+    </div>
+  );
+};
+
+export default Add;
--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -1,14 +1,15 @@
-import { Navbar } from "./_components/navbar";
 import MobileNavbar from "@/components/shared/navbar/navbar";
+
+import { Navbar } from "./_components/navbar";
 
 const ProtectedLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="relative inset-0 pb-5 px-10 h-full w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]">
       <div className="absolute pointer-events-none bottom-0 left-0 flex right-0 top-0 bg-[radial-gradient(circle_500px_at_50%_200px,#C9EBFF,transparent)]"></div>
       <Navbar />
-      {children}
+      <main className="relative pt-5 z-10"> {children}</main>
       <div className="block lg:hidden">
-        <MobileNavbar/>
+        <MobileNavbar />
       </div>
     </div>
   );
