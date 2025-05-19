9fd302b55a256043c4087181c712bc4ea1959fae
Szymon <szymongrzesiak.pl@gmail.com>
2025-05-19 20:27:45 +0200

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -197,7 +197,7 @@ export default function Home() {
       </main>
       <footer className="flex bg-slate-800 text-white flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t">
         <p className="text-xs">
-          &copy; 2024 Language Learning. All rights reserved.
+          &copy; 2025 Language Learning. All rights reserved.
         </p>
         <nav className="sm:ml-auto flex gap-4 sm:gap-6">
           <Link
